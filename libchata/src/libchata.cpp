73ee12b80506aa1ad6d9a2e95eaa9c179113def2
Slackadays <lightingdzeyenr@gmail.com>
2024-11-04 11:19:27 -0500

    Memory pool

--- libchata/src/libchata.cpp
+++ libchata/src/libchata.cpp
@@ -1,13 +1,33 @@
 #include "libchata.hpp"
+#include <print>
+#include <vector>
 
-
+constexpr std::string_view libchata_version_str = PROJECT_VERSION;
 
 std::optional<ChataError> ChataProcessor::process_data(float& in1) {
     in1 *= 0.5;
     return std::nullopt;
 }
 
+/*std::array<char> process_injections(const std::span<InputFile> files) {
+    std::array<char> injections;
+    for (const auto& file : files) {
+        for (const auto& c : file.data) {
+            injections.push_back(c);
+        }
+    }
+    return injections;
+}*/
+
+
 std::optional<ChataError> ChataProcessor::compile(const std::span<InputFile> files) {
+    std::vector<int, MemoryPool<int>> test;
+    test.push_back(1);
+    test.push_back(2);
+    test.push_back(3);
+    for (const auto& i : test) {
+        std::println("{}", i);
+    } 
     return std::nullopt;
 }
 
--- libchata/src/libchata.hpp
+++ libchata/src/libchata.hpp
@@ -2,11 +2,38 @@
 #include <optional>
 #include <string_view>
 #include <array>
-#include <cstring>
+#include <cstdint>
 #include <span>
+#include <memory>
 
-constexpr size_t max_code_size = 1048576;
-constexpr std::string_view libchata_version_str = PROJECT_VERSION;
+constexpr size_t libchata_memory_pool_size = 4194304;
+
+template <class T>
+class MemoryPool {
+    std::array<std::byte, libchata_memory_pool_size> pool;
+    size_t used = 0;
+
+public:
+    using value_type = T;
+
+    MemoryPool() = default;
+
+    [[nodiscard]] T* allocate(size_t requested) {
+        std::size_t bytes = requested * sizeof(T);
+        if (bytes + used > pool.size()) {
+            return nullptr;
+        }
+        T* ptr = reinterpret_cast<T*>(pool.data() + used);
+        used += bytes;
+        return ptr;
+    }
+
+    void deallocate(T* ptr, size_t requested) {
+        return;
+    }
+};
+
+
 
 enum class ErrorType {
     Nada,
@@ -30,7 +57,8 @@ public:
 };
 
 class ChataProcessor {
-    std::array<unsigned char, max_code_size> assembled_code;
+    //std::array<char, libchata_code_memory_size> code_memory;
+    //std::array<std::byte, libchata_executable_memory_size> executable_memory;
 
 public:
     std::optional<ChataError> compile(const std::string_view& code);
